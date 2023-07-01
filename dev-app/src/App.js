02b9d3ed434b589173d7a9b45245a14fae096b33
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-02 01:30:47 +0530

    initial commit

---
+++ dev-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SandBox from "./components/SandBox";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <div class="row">
+
+          <SandBox/>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
