e5cedb5c81b8f434d0bcd54ec2679ee39b065bd1
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-02 11:12:26 +0530

    endpoint with http client using useeffect hook

--- dev-app/src/App.js
+++ dev-app/src/App.js
@@ -1,15 +1,42 @@
-import React from "react";
-import SandBox from "./components/SandBox";
+import React, { useState, useEffect } from "react";
+import httpClient from "./api/httpClient";
+import Spinner from './components/Spinner';
+import Alert from './components/Alert'
+
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    const getAllUsers = async () => {
+      try {
+        const { data } = await httpClient.get("/users");
+        setUsers(data);
+      } catch ({ message }) {
+        setError(message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getAllUsers();
+  },[]);
+
+
+  if (loading) return <Spinner color="primary"/>
+  if (error) return <Alert title="Network Error" body={error} color="danger" />
+  
+  const renderUsrs = users ? users : null;
+  
+
   return (
     <>
       <div className="container">
-        <div class="row">
-
-          <SandBox/>
-
-        </div>
+        <div className="row">{renderUsrs.length}</div>
       </div>
     </>
   );
