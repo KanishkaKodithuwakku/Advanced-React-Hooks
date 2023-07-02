06b13ee4034112123f26e7cbf1f7ade7c3c18697
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-02 17:04:43 +0530

    ui fixed

--- dev-app/src/Sanbox/FilterDataHook.js
+++ dev-app/src/Sanbox/FilterDataHook.js
@@ -2,20 +2,34 @@ import React from "react";
 import useSearchData from "../hooks/useSearchData";
 import Spinner from "../components/Spinner/Spinner";
 import { Alert } from "bootstrap";
+import ListItem from "../components/List/ListItem";
+import ListBody from "../components/List/ListBody";
 
-const FilterDataHook = () => { 
+const FilterDataHook = () => {
+  const [loading, error, data] = useSearchData("Rick", "Male");
+  if (loading) return <Spinner color="danger" />;
+  if (error) return <Alert title="Searching Error" body={error} />;
 
-    const [loading, error, data] = useSearchData("Rick","Male");
-    if (loading) return <Spinner color="danger" />;
-    if (error) return <Alert title="Searching Error" body={error} />;
+  const renderData = data ? (
+    data.map((chr) => <ListItem key={chr.id} body={chr.name}></ListItem>)
+  ) : (
+    <Alert title="User Not Found" />
+  );
 
-    const renderData = data ? (
-      data.map((chr) => <p key={chr.id}>{chr.name}</p>)
-    ) : (
-      <Alert title="User Not Found" />
-    );
-    
-    return <>{renderData}</>;
-}
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <ListBody>
+              <ListItem status="active" body="Charactres" />
+              {renderData}
+            </ListBody>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default FilterDataHook;
