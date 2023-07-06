709de8cb2f5283290d56e3b4b42e5ec890fcb8e6
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-06 16:12:55 +0530

    change start script in package json

--- dev-app/src/App.js
+++ dev-app/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CustomHook from "./Sanbox/CustomHook";
 import FilterDataHook from "./Sanbox/FilterDataHook";
+import Navigation from "./components/Layouts/Navigation";
 
 const App = () => {
 
   
   return (
     <>
+      <Navigation/>
       {/* <CustomHook/> */}
       <FilterDataHook/>
     </>
